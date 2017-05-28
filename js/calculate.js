88794f023055b4049c5db498d4914a1b8d722cde
saba <takoyaki8989@jcom.zaq.ne.jp>
2017-05-28 18:06:05 +0900

    first commit

---
+++ js/calculate.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // formのsubmit時の動作を定義する
+  document.getElementById("calc-form").onsubmit = () => {
+    // コメントを入力するinputを取得する
+    const formulaInput = document.getElementById("calc-input");
+    formulaInput.value = eval(formulaInput.value);
+    return false;
+  };
+});
+
+function pressedValue(button) {
+  const formulaInput = document.getElementById("calc-input");
+  var lastFormula = formulaInput.value.slice(-1);
+  if(button === 'C') {
+    formulaInput.value = "";
+  }
+  else if(isOper(lastFormula) && lastFormula === button) {
+  }
+  else {
+    formulaInput.value += button;
+  }
+}
+
+function isOper(letter) {
+  if(letter === '+')
+    return true;
+  else if(letter === '-') {
+    return true;
+  }
+  else if(letter === '*') {
+    return true;
+  }
+  else if(letter === '/') {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
